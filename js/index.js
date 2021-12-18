08b1fe35dd389034d979edd1129a183e8269b9df
ScorpioXKiller <dima.gorodov1999@gmail.com>
2021-12-19 00:36:10 +0200

    implement interactive for popup.

---
+++ js/index.js
@@ -0,0 +1,49 @@
+let editFormBtn = document.getElementById('edit');
+let pageOverlay = document.querySelector('.page__overlay');
+let popup = document.querySelector('.popup');
+let closeFormBtn = popup.querySelector('.popup__close-button');
+let saveFormBtn = popup.querySelector('.popup__save-button');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let nameInput = popup.querySelector('.popup__item_el_user-name');
+let aboutInput = popup.querySelector('.popup__item_el_user-about');
+
+let openForm = () => {
+    popup.style.display = "block";
+}
+
+let closeForm = () => {
+    popup.style.display = "none";
+}
+
+let enablePageOverlay = () => {
+    pageOverlay.style.display = "block";
+}
+
+let disablePageOverlay = () => {
+    pageOverlay.style.display = "none";
+}
+
+function handleProfileFormSubmit(evt){
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+}
+
+function tuggleForm(){
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
+}
+
+editFormBtn.addEventListener('click', openForm);
+editFormBtn.addEventListener('click', enablePageOverlay);
+
+closeFormBtn.addEventListener('click', closeForm);
+closeFormBtn.addEventListener('click', disablePageOverlay);
+
+saveFormBtn.addEventListener('click', closeForm);
+saveFormBtn.addEventListener('click', disablePageOverlay);
+
+tuggleForm();
+popup.addEventListener('submit', handleProfileFormSubmit);
