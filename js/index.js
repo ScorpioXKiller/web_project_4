558eaec4be749404160518ef88809eac27f8c288
ScorpioXKiller <dima.gorodov1999@gmail.com>
2021-12-22 05:25:36 +0200

    fix popup window.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-let openFormBtn = document.getElementById('edit');
+let openPopupBtn = document.getElementById('edit');
 let popup = document.querySelector('.popup');
-let saveFormBtn = popup.querySelector('.popup__save-button');
+let popupForm = popup.querySelector('.popup__form');
 let closeFormBtn = popup.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let nameInput = popup.querySelector('.popup__item_el_user-name');
 let aboutInput = popup.querySelector('.popup__item_el_user-about');
 
-let openForm = () => {
+let openPopup = () => {
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
 
     popup.classList.toggle('popup_visible');
 }
 
-let closeForm = () => {
+let closePopup = () => {
     popup.classList.toggle('popup_visible');
 }
 
@@ -24,9 +24,9 @@ function handleProfileFormSubmit(evt){
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
     
-    closeForm();
+    closePopup();
 }
 
-openFormBtn.addEventListener('click', openForm);
-saveFormBtn.addEventListener('click', handleProfileFormSubmit);
-closeFormBtn.addEventListener('click', closeForm);
+openPopupBtn.addEventListener('click', openPopup);
+closeFormBtn.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', handleProfileFormSubmit);
